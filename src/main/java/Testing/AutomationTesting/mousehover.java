3908c05daa30931672dfc612c038b43131a445b8
Suraj931525 <surajpatil9315@gmail.com>
2026-09-04 11:41:36 +0530

    dragAndDrop action performing code

--- src/main/java/Testing/AutomationTesting/mousehover.java
+++ src/main/java/Testing/AutomationTesting/mousehover.java
@@ -7,16 +7,17 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class mousehover {
-
+	 EdgeDriver driver;
 	public static void main(String[] args) {
-		 EdgeDriver driver;
+		
 		
 		mousehover ob =new mousehover ();
 		ob.mousehover1();
+		ob.dragAndDrop();
 	}
 	
 	void mousehover1() {
-		EdgeDriver driver = new EdgeDriver();
+		 driver = new EdgeDriver();
 
         driver.manage().window().maximize();
         driver.get("https://testautomationpractice.blogspot.com/");
@@ -30,6 +31,15 @@ public class mousehover {
         ac.moveToElement(pointme).build().perform();
 	}
 	
+	public void dragAndDrop() {
+
+	    WebElement drag = driver.findElement(By.id("draggable"));
+	    WebElement drop = driver.findElement(By.id("droppable"));
+
+	    Actions actions = new Actions(driver);
+
+	    actions.dragAndDrop(drag, drop).perform();
+	}
 	
 			
 			
